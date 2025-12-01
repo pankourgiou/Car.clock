f659a4d3bc2cb89cbb5896af20820c7855aa851a
L <60981745+pankourgiou@users.noreply.github.com>
2025-12-01 18:28:32 +0200

    Update car_clock.py

--- car_clock.py
+++ car_clock.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # List of OS/distro names corresponding to each hour
 os_names = [
     "Mercedes", "Peugeot", "Volkswagen", "KIA", "Renault", "Hyundai",
-    "Audi", "McLaren", "McLaren", "McLaren", "Fiat", "BMW"
+    "Audi", "Honda", "Hyundai", "McLaren", "Fiat", "BMW"
 ]
 
 def draw_os_clock():
